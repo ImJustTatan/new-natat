af1f4f8875c5d2a4820b5defaeaf486315ae989b
ImJustTatan <sebastianrh27.12@gmail.com>
2018-09-26 19:27:48 -0400

    can the typos stop please

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os # for env vars
 import discord
 from discord.ext import commands
 
-token = os.environ.get('TOKEN', None)
+token = os.environ.get('TOKEN')
 bot = commands.Bot(command_prefix='$')
 
 @bot.event
