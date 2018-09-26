01ce2b975adceb618fd3f0e88db6f6ec0a003ff9
ImJustTatan <sebastianrh27.12@gmail.com>
2018-09-26 18:52:21 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os # for env vars
+import discord
+from discord.ext import commands
+
+token = os.environment.get('TOKEN', None)
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send("boyyoass")
+
+bot.run(token)
