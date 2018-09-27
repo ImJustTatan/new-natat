beb2edc7b2faa45eb71ce0d79abadafb1f3b46aa
ImJustTatan <sebastianrh27.12@gmail.com>
2018-09-26 23:07:38 -0400

    added subcommand group

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 token = os.environ.get('TOKEN')
 print('token: {}'.format(token))
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='>')
 
 @bot.event
 async def on_ready():
@@ -16,5 +16,15 @@ async def on_ready():
 @bot.command()
 async def greet(ctx):
     await ctx.send("boyyoass")
+
+@bot.group()
+async def config(ctx):
+    if ctx.user.id == tatanID:
+        if ctx.invoked_subcommand is None:
+            await ctx.send("no subcommand was called")
+        else:
+            pass
+    else:
+        await ctx.send("don\'t fuckin touch my configs")
 
 bot.run(token)
