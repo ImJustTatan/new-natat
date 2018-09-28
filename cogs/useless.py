72f5f86475c78a7b2b75eeda6b9ea23c6c39483b
ImJustTatan <sebastianrh27.12@gmail.com>
2018-09-28 17:57:45 -0400

    cogs still not working

--- cogs/useless.py
+++ cogs/useless.py
@@ -1,19 +1,19 @@
 import discord
-from discord.ext import commands as bot
+from discord.ext import commands
 import random
 
 class Fun:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@bot.command(description='A simple command for testing the bot.', 
+	@command.command(description='A simple command for testing the bot.', 
 					  aliases=['hello', 'salute', 'test'])
 	async def greet(ctx):
 		"""Greets you. Mainly used for testing."""
 		await ctx.send(random.choice(["boyyoass", "heyo", "what do you want"]))
 
-	@bot.command()
-	@bot.guild_only()
+	@command.command()
+	@command.guild_only()
 	async def joined(self, ctx, *, member: discord.Member):
 		"""Says when a member joined."""
 		await ctx.send(f'{member.display_name} joined on {member.joined_at}')
