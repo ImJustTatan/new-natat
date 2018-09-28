71e9791736ef93e59d6f5cd43f807595d10ab8d3
ImJustTatan <sebastianrh27.12@gmail.com>
2018-09-28 18:03:00 -0400

    fixed lil typo, cogs still not working

--- cogs/useless.py
+++ cogs/useless.py
@@ -6,14 +6,14 @@ class Fun:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@command.command(description='A simple command for testing the bot.', 
+	@commands.command(description='A simple command for testing the bot.', 
 					  aliases=['hello', 'salute', 'test'])
 	async def greet(ctx):
 		"""Greets you. Mainly used for testing."""
 		await ctx.send(random.choice(["boyyoass", "heyo", "what do you want"]))
 
-	@command.command()
-	@command.guild_only()
+	@commands.command()
+	@commands.guild_only()
 	async def joined(self, ctx, *, member: discord.Member):
 		"""Says when a member joined."""
 		await ctx.send(f'{member.display_name} joined on {member.joined_at}')
