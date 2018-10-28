6d65befebf6590575c9d32bf3dfd38bd2a466528
ImJustTatan <sebastianrh27.12@gmail.com>
2018-10-27 19:40:34 -0500

    added BytesIO support for files good lord it took me hours to figure this out

--- cogs/members.py
+++ cogs/members.py
@@ -1,16 +1,22 @@
 import discord
 from discord.ext import commands
+import aiohttp
+
+from io import BytesIO
 import random
 
 class Members:
 	def __init__(self, bot):
 		self.bot = bot
+		self.session = aiohttp.ClientSession(loop=bot.loop)
 
 	@commands.command(aliases=['joinedat', 'joined_at'])
 	@commands.guild_only()
-	async def joined(self, ctx, *, member: discord.Member):
+	async def joined(self, ctx, *, member: discord.Member = None):
 		"""Says when a member joined."""
-		if member != ctx.author:
+		if member is None:
+			await ctx.send(f'you joined on {ctx.author.joined_at}')
+		else:
 			await ctx.send(f'{member.display_name} joined on {member.joined_at}')
 
 	@commands.command(aliases=['pfp', 'profilepic', 'avatarpic', 
@@ -18,8 +24,15 @@ class Members:
 	async def avatar(self, ctx, *, member: discord.Member = None):
 		"""Messages a member's avatar url back."""
 		if member is None:
-			await ctx.send(content=f'here\'s your uglyass avatar {ctx.author.avatar_url}')
+			async with self.session.get(ctx.author.avatar_url) as resp:
+				buffer = BytesIO(await resp.read())
+				avatar = discord.File(fp=buffer, filename='avatar.webp')
+				await ctx.send(content='here\'s your uglyass avatar', file=avatar)
 		else:
-			await ctx.send(content=f'here\'s {member.display_name}\'s uglyass avatar {member.avatar_url}')
+			async with self.session.get(member.avatar_url) as resp:
+				buffer = BytesIO(await resp.read())
+				avatar = discord.File(fp=buffer, filename='avatar.webp')
+				await ctx.send(content=f'here\'s {member.display_name}\'s uglyass avatar', file=avatar)
+
 def setup(bot):
 	bot.add_cog(Members(bot))
